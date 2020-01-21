84262a5d2bf639919acd4845657586cfed4d43f7
Indigo Richards <thedomesticdingo@gmail.com>
2020-01-21 13:45:16 -0800

    MVP
    should be MVP

--- server.js
+++ server.js
@@ -6,4 +6,69 @@ const server = express();
 
 server.use(express.json());
 
+//  V ENDPOINTS V
+
+server.get('/', (req, res) => {
+    res.send("It's alive!")
+})
+
+//Get all accounts
+server.get('/accounts', (req, res) => {
+    db.get()
+        .then(acc => res.status(200).json(acc))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "There was an error getting the accounts." });
+        })
+})
+
+//Get account by ID
+server.get('/accounts/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.get(id)
+        .then(acc => res.status(200).json(acc))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "There was an error getting the account by ID." })
+        })
+})
+
+//Add new account
+server.post('/accounts', (req, res) => {
+    const acc = req.body;
+
+    db.insert(acc)
+        .then(acc => res.status(200).json(acc))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "There was an error adding a new account." })
+        })
+})
+
+//Update account information
+server.put('/accounts/:id', (req, res) => {
+    const acc = req.params.id;
+    const update = req.body;
+
+    db.update(acc, update)
+        .then(acc => res.status(200).json(acc))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "There was an error editing the account." })
+        })
+})
+
+//Delete an account
+server.delete('/accounts/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.remove(id)
+        .then(acc => res.status(200).json(acc))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "There was an error deleting the account." })
+        })
+})
+
 module.exports = server;
